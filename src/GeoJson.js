99b3372053a114a8c4ec6b071a1d542dcc99d3d6
Kei6327 <gzhongtian845@gmail.com>
2022-12-23 13:52:32 +0900

    各ファイルの追加

--- src/GeoJson.js
+++ src/GeoJson.js
@@ -1,81 +1,66 @@
 const GeoJson = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {
-                "marker-image": "dist//g-marker.svg",
-                "title": "奥の院",
-                "description":"奥の院の説明です",
-                "website":"src/quizs/00.html"
-            },
-            "geometry": {
-                "coordinates": [
-                    135.60360796287182,
-                    34.21742878512801
-                ],
-            "type": "Point"
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "marker-color": "#7e7e7e",
-                "marker-size": "medium",
-                "marker-symbol": "",
-                "marker-image": "dist//marker.svg",
-                "title": "貴志駅"
-            },
-            "geometry": {
-                "coordinates": [
-                    135.3121040002124,
-                    34.20931501069661
-                ],
-            "type": "Point"
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "marker-image": "dist//marker.svg",
-                "title": "潮岬"
-            },
-            "geometry": {
-                "coordinates": [
-                    135.75578424931342,
-                    33.43697963106372
-                ],
-            "type": "Point"
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "marker-image": "dist//marker.svg",
-                "title": "ぶつぶつ川"
-            },
-            "geometry": {
-                "coordinates": [
-                    135.9160948572283,
-                    33.57188563714874
-                ],
-            "type": "Point"
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "marker-image": "dist//marker.svg",
-                "title": "那智の滝",
-                "description":"日本一の落差を"
-            },
-            "geometry": {
-            "coordinates": [
-                135.8897592331021,
-                33.67306193486884
-            ],
-            "type": "Point"
-            }
-        }
-    ]
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        "marker-image": "dist//g-marker.svg",
+        title: "奥の院",
+        description: "弘法大師空海が開創した聖地",
+        website: "src/quizs/00.html",
+      },
+      geometry: {
+        coordinates: [135.60360796287182, 34.21742878512801],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "marker-image": "dist//marker.svg",
+        title: "貴志駅",
+      },
+      geometry: {
+        coordinates: [135.3121040002124, 34.20931501069661],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        "marker-image": "dist//marker.svg",
+        title: "潮岬",
+      },
+      geometry: {
+        coordinates: [135.75578424931342, 33.43697963106372],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        "marker-image": "dist//marker.svg",
+        title: "ぶつぶつ川",
+      },
+      geometry: {
+        coordinates: [135.9160948572283, 33.57188563714874],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        "marker-image": "dist//marker.svg",
+        title: "那智の滝",
+        description: "日本一の落差を",
+      },
+      geometry: {
+        coordinates: [135.8897592331021, 33.67306193486884],
+        type: "Point",
+      },
+    },
+  ],
 };
