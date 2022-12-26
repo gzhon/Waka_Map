bbc88d4d44a91813fdd87a69a8cea529b68903ae
Kei6327 <gzhongtian845@gmail.com>
2022-12-27 08:59:43 +0900

    問題修正

--- src/GeoJson.js
+++ src/GeoJson.js
@@ -21,6 +21,7 @@ const GeoJson = {
         "marker-size": "medium",
         "marker-symbol": "",
         "marker-image": "dist//marker.svg",
+        description: "世界でも有名な猫駅長がいる駅",
         website: "https://gzhon.github.io/Waka_Map/src/quizs/quiz.html?id=01",
         title: "貴志駅",
       },
@@ -35,6 +36,7 @@ const GeoJson = {
         "marker-image": "dist//marker.svg",
         website: "https://gzhon.github.io/Waka_Map/src/quizs/quiz.html?id=02",
         title: "潮岬",
+        description: "",
       },
       geometry: {
         coordinates: [135.75578424931342, 33.43697963106372],
@@ -47,6 +49,7 @@ const GeoJson = {
         "marker-image": "dist//marker.svg",
         website: "https://gzhon.github.io/Waka_Map/src/quizs/quiz.html?id=04",
         title: "ぶつぶつ川",
+        description: "日本で最も短い川",
       },
       geometry: {
         coordinates: [135.9160948572283, 33.57188563714874],
@@ -57,12 +60,25 @@ const GeoJson = {
       type: "Feature",
       properties: {
         "marker-image": "dist//marker.svg",
-        title: "那智の滝",
-        website: "https://gzhon.github.io/Waka_Map/src/quizs/quiz.html?id=04",
-        description: "日本一の落差を",
+        title: "日本初のある施設",
+        website: "https://gzhon.github.io/Waka_Map/src/quizs/quiz.html?id=02",
+        description: "小型ロケットで宇宙へ",
       },
       geometry: {
         coordinates: [135.8897592331021, 33.67306193486884],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        "marker-image": "dist//marker.svg",
+        title: "落差が日本一の那智の滝",
+        website: "https://gzhon.github.io/Waka_Map/src/quizs/quiz.html?id=03",
+        description: "紀伊山地の豊かな自然",
+      },
+      geometry: {
+        coordinates: [135.8874922, 33.6753725],
         type: "Point",
       },
     },
